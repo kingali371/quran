be153de09a21cf1fd320526ab945197454a72e50
Zaid _ <117419326+x72x@users.noreply.github.com>
2022-12-07 19:20:44 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
-apt update && apt upgrade -y
-apt install python3-pip -y
 apt install ffmpeg -y
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt-get install -y nodejs
 npm i -g npm
-pip3 install --upgrade pip
-pip3 install -U -r requirements.txt
-
 gunicorn app:app
-
