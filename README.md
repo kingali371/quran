23abc75563aad416548adef3773178c0c0281e9b
Zaid _ <117419326+x72x@users.noreply.github.com>
2022-12-07 19:14:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# QuranRadio
+[Deploy To Heroku](https://dashboard.heroku.com/new?template=https://github.com/x72x/quran)
---
+++ app.py
@@ -0,0 +1,62 @@
+import asyncio, os
+
+from pyrogram import Client
+
+from pytgcalls import PyTgCalls 
+
+from pytgcalls import idle
+
+from pytgcalls import StreamType
+
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+
+from pytgcalls.types.input_stream.quality import HighQualityAudio,    HighQualityVideo,    LowQualityVideo,    MediumQualityVideo
+
+from pytube import YouTube
+
+import aiohttp
+
+from Python_ARQ import ARQ
+
+from pyrogram.types import Message
+
+ARQ_API_KEY = "HMPXNS-BDPCCB-UJKRPU-OQADHG-ARQ"
+
+aiohttpsession = aiohttp.ClientSession()
+
+arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
+
+API_ID = int(os.getenv("API_ID"))
+CHAT_ID = int(os.getenv("CHAT_ID"))
+API_HASH = os.getenv("API_HASH")
+
+
+SESSION = os.getenv("SESSION")
+
+app = Client(
+
+    "MyBot",
+
+    api_id=API_ID,
+
+    api_hash=API_HASH,
+    
+    session_string=SESSION
+
+)
+
+from flask import Flask
+
+site = Flask(__name__)
+call_py = PyTgCalls(app)
+
+@site.route("/")
+def hello():
+  return "Hi"
+
+call_py.start()
+threading.Thread(target=site.run, daemon=True).start()
+print("✅")
+while True:
+   call_py.join_group_call(CHAT_ID,AudioPiped("https://bit.ly/3OWfmUp"),stream_type=StreamType().pulse_stream,)
+   asyncio.sleep(82753+10)
